83fd8d202fcdc85efc5cf81191da2078d6e3574a
Gullies <1448991007@qq.com>
2020-07-19 22:29:59 +0800

    temp2

--- apis/views.py
+++ apis/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def hello(request):
-    # return HttpResponse("hello world")
-    return render(request, 'index.html')
+    return HttpResponse("hello world")
+    # return render(request, 'index.html')
