45660e4754cd8d489b250bb190f144de3f0f7973
Gullies <1448991007@qq.com>
2020-08-07 06:58:47 +0800

    temp4

--- django_server/settings.py
+++ django_server/settings.py
@@ -118,12 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# STATIC_URL = '/static/'
-STATIC_URL = '/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, "apis/templates/static"),
-    os.path.join(BASE_DIR, "apis/templates"),
+    os.path.join(BASE_DIR, "apis/templates/static"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECURE_SSL_REDIRECT = True
