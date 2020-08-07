a9dbabbd2e71345b26c5f5f5271c3764f9095cf6
Artemisia <lanbinzheng@qq.com>
2020-08-07 16:20:10 +0800

    temp14

--- django_server/urls.py
+++ django_server/urls.py
@@ -18,8 +18,8 @@ from django.urls import path
 from apis import views as apis_views
 ## Solve the problem that Django React cannot find /favicon.ico
 # from django.contrib.staticfiles.views import serve
-from django.contrib.staticfiles.storage import staticfiles_storage
-from django.views.generic.base import RedirectView
+# from django.contrib.staticfiles.storage import staticfiles_storage
+# from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +27,12 @@ urlpatterns = [
     ## Solve the problem that Django React cannot find /favicon.ico
     # path('favicon.ico', serve, {'path': 'favicon.ico'}),
     # path('logo192.png', serve, {'path': 'logo192.png'}),
-    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'))),
-    path('logo192.png', RedirectView.as_view(url=staticfiles_storage.url('logo192.png'))),
-]
+    # path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'))),
+    # path('logo192.png', RedirectView.as_view(url=staticfiles_storage.url('logo192.png'))),
+] 
+
+##
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
