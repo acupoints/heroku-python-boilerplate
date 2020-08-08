72a1083171d0425778afd2825e30a7c453942cda
Gullies <1448991007@qq.com>
2020-08-09 01:17:56 +0800

    modified base import

--- django_server/settings/development.py
+++ django_server/settings/development.py
@@ -1,4 +1,5 @@
-from .base import * #NOQA
+# from .base import * #NOQA
+from ..settings_original import *
 
 # Modify settings in development mode
 SECURE_SSL_REDIRECT = False
--- django_server/settings/production.py
+++ django_server/settings/production.py
@@ -1,4 +1,5 @@
-from .base import * #NOQA
+# from .base import * #NOQA
+from ..settings_original import *
 
 # Modify settings in production mode
 SECURE_SSL_REDIRECT = True
